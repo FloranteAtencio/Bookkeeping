101a39c107623b89f664ba068da97eab1e1e498d
Florante Atencio <144699572+FloranteAtencio@users.noreply.github.com>
2025-01-28 00:56:08 +0800

    Create Incrementalbackup.sh

---
+++ Incrementalbackup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+export ORACLE_SID=FREE
+export ORACLE_HOME=/opt/oracle/product/23ai/dbhomeFree
+
+rman target / <<EOF
+RUN {
+  BACKUP INCREMENTAL LEVEL 1 DATABASE FORMAT '/home/oracle/backup/incremental_backup_%d_%T';
+  BACKUP PLUGGABLE DATABASE ALL FORMAT '/home/oracle/backup/pluggable_incremental_backup_%d_%T'
+    FILE_NAME_CONVERT=('/opt/oracle/oradata/FREE', '/Home/Oracle/Backup/FREE');
+  DELETE OBSOLETE;
+}
+EOF
+
+# Remove backups older than 7 days
+find "/home/oracle/backup" -name "pluggable_incremental_backup_*" -type f -mtime +7 -exec rm {} \;
+find "//home/oracle/backup" -name "incremental_backup_*" -type f -mtime +7 -exec rm {} \;
+# backup shell file located in /Home/Oracle/Script/
